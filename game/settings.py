66b5ca10c29d6627b33fdf2fa7c312759886aad6
merumraviteja <raviteja87731@gmail.com>
2020-02-12 22:55:47 +0530

    ok

--- game/settings.py
+++ game/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'game.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
+django_heroku.settings(locals())
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
